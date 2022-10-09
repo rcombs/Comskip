6789fd255672bb31204968810b0aebd9ce9807f5
behinder <seba.szwarc at gmail.com>
2022-10-09 20:22:33 +0200

    Defined PROCESS_CC (maybe)

--- comskip.h
+++ comskip.h
@@ -6,5 +6,13 @@
 #endif
 #define _UNICODE
 
+#ifndef PACKAGE_STRING
+#define PACKAGE_STRING " Comskip 0.82.011"
+#endif
+
 #define MAXWIDTH	8000
 #define MAXHEIGHT	4800
+
+#ifndef PROCESS_CC
+#define PROCESS_CC "true"
+#endif
